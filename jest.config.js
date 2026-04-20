db85acbf193aafecaec6f730d5eadb6b12424876
GUT41 <christianvilleranque19@gmail.com>
2026-04-21 07:12:07 +0800

    fix(deps): downgrade Jest to 29 for compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: 'jest-expo',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest'],
-  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
 };
